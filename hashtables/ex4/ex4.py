64a03516ff89fba4d25d7add52ea8f9aed296252
jarrodskahill <jarrodskahill@gmail.com>
2020-06-08 13:08:08 -0700

    finished strech

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,47 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    numbers = {}
+    num = 0
+    for x, y in enumerate(a, 0):
+        numbers[x] = y
 
+    result = []
+    while num != len(numbers):
+        print(numbers[num])
+        if numbers[num] == 0:
+            num += 1
+
+        elif numbers[num] > 0:
+            if numbers[num] in result:
+                num += 1
+            elif numbers[num] * -1 in numbers.values():
+                result.append(abs(numbers[num]))
+                num += 1
+            else:
+                num += 1
+
+        elif numbers[num] < 0:
+            if abs(numbers[num]) in result:
+                num += 1
+
+            elif abs(numbers[num]) in a:
+                result.append(abs(numbers[num]))
+                num += 1
+
+    # elif numbers[num] > 0:
+    #     if int('-' + str(numbers[num])) in a:
+    #         result.append(abs(numbers[num]))
+    # while len(numbers) != num:
+    #     if numbers[num] < 0:
+    #         if abs(numbers[num]) in a:
+    #             result.append(abs(numbers[num])
+
+    #     elif numbers[num] > 0:
+    #         if int('-' + str(numbers[num])) in a:
+    #             result.append(numbers[num])
+
+    #     return num += 1
     return result
 
 
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,19 @@
 # Your code here
 
 
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+    compFile = dict(files=files, queries=queries)
+
+    for x in compFile.get('files'):
+        if (x[-3:]) in compFile.get('queries'):
+            result.append(x)
+        else:
+            continue
 
     return result
 
